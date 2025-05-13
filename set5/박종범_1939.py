145e52d22024a6a796d80280da52a70081fe0881
parkjbdev <dev@parkjb.com>
2025-05-13 11:25:15 +0900

    1939: add comments for dijkstra

--- set5/박종범_1939.py
+++ set5/박종범_1939.py
@@ -14,6 +14,7 @@ S -= 1
 E -= 1
 
 
+# Dijkstra
 limits = [0.0 for _ in range(N)]
 limits[S] = inf
 
@@ -23,14 +24,15 @@ while pq:
     limit, v = heapq.heappop(pq)
     limit = -limit
 
+    # 우선순위 큐에서 중량이 가장 큰 순으로 탐색하기 때문에,
+    # 처음으로 도달할 때가 최대 중량 경로임이 보장됨
     if v == E:
         print(limit)
         break
 
     for nv, nl in ADJ[v]:
-        cmp_l = min(limit, nl)
+        cmp_l = min(limit, nl)  # 최대 중량은 경로상 제한중량의 최솟값
 
-        if cmp_l > limits[nv]:
+        if cmp_l > limits[nv]:  # 기존의 최대중량보다 크면 갱신
             limits[nv] = cmp_l
             heapq.heappush(pq, (-cmp_l, nv))
-
