e9d55893bc6a56118463f58ee2768f13ab7c3d8e
gomhyeok <gomhyeok@gmail.com>
2025-05-19 02:18:38 +0900

    백준 내리막길 - BFS로 풀이해보려 했으나 누적 dp가 항상 최신 값이 아니라는 문제 발생 - 따라서 dfs + dp로 문제 풀이

---
+++ set5/내리막길.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include <stack>
+
+using namespace std;
+
+int dx[4] = {1, -1, 0, 0};
+int dy[4] = {0, 0, 1, -1};
+int n, m;
+long long board[501][501];
+long long possible[501][501];
+
+long long dfs(int x, int y);
+
+int main() {
+    cin>>n>>m;
+
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<m; j++) {
+            cin>>board[i][j];
+            possible[i][j] = -1;
+        }
+    }
+
+    cout<<dfs(0, 0)<<endl;
+}
+
+long long dfs(int x, int y) {
+    if(x == n-1 && y == m-1) return 1;
+    if(possible[x][y] != -1) return possible[x][y];
+
+    possible[x][y] = 0;
+
+    for(int i=0; i<4; i++) {
+        int nx = x + dx[i];
+        int ny = y + dy[i];
+
+        if(nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
+        if(board[nx][ny] >= board[x][y]) continue;
+
+        possible[x][y] += dfs(nx, ny);
+    }
+
+    return possible[x][y];
+}
