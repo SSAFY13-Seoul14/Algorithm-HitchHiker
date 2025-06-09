87af20a096577b35daa63c04efc15abcff39f1f5
parkjbdev <dev@parkjb.com>
2025-06-09 17:29:35 +0900

    feat: impl generic segment tree

--- set7/C5676/src/main.rs
+++ set7/C5676/src/main.rs
@@ -1,112 +1,133 @@
 use std::{
     fmt::Write,
     io::{self, Read},
-    ops::RangeInclusive,
 };
 
-struct SegmentTree {
-    tree: Vec<i32>,
-    n: usize,
-}
+mod segtree {
+    use std::ops::RangeInclusive;
 
-impl SegmentTree {
-    pub fn new(x: &Vec<i32>) -> Self {
-        let tree_size = x.len().next_power_of_two() << 1;
-        let mut tree = Self {
-            tree: vec![1; tree_size],
-            n: x.len(),
-        };
-
-        tree.build(x);
-
-        tree
+    pub struct SegmentTree<T, F>
+    where
+        T: Clone + Copy,
+        F: Fn(&T, &T) -> T,
+    {
+        tree: Vec<T>,
+        n: usize,
+        default_value: T,
+        operation: F,
     }
 
-    pub fn build(&mut self, x: &Vec<i32>) {
-        self._build(x, 0, self.n - 1, 1);
-    }
+    impl<T: Clone + Copy, F: Fn(&T, &T) -> T> SegmentTree<T, F> {
+        pub fn new(x: &Vec<T>, default_value: T, operation: F) -> Self {
+            let tree_size = x.len().next_power_of_two() << 1;
+            let mut tree = Self {
+                tree: vec![default_value.clone(); tree_size],
+                n: x.len(),
+                default_value,
+                operation,
+            };
 
-    pub fn update(&mut self, idx: usize, value: i32) {
-        self._update(0, self.n - 1, idx, value, 1);
-    }
+            tree.build(x);
 
-    pub fn query(&self, range: &RangeInclusive<usize>) -> i32 {
-        return self._query(0, self.n - 1, range, 1);
-    }
-
-    fn _build(&mut self, x: &Vec<i32>, start_idx: usize, end_idx: usize, tree_idx: usize) {
-        if tree_idx >= self.tree.len() {
-            return;
+            tree
         }
 
-        if start_idx == end_idx {
-            self.tree[tree_idx] = x[start_idx];
-            return;
+        fn build(&mut self, x: &Vec<T>) {
+            self._build(x, 0, self.n - 1, 1);
         }
 
-        let mid_idx = (start_idx + end_idx) / 2;
-        let left_child_idx = tree_idx * 2;
-        let right_child_idx = tree_idx * 2 + 1;
-
-        self._build(x, start_idx, mid_idx, left_child_idx);
-        self._build(x, mid_idx + 1, end_idx, right_child_idx);
-
-        self.tree[tree_idx] = self.tree.get(left_child_idx).unwrap_or(&1)
-            * self.tree.get(right_child_idx).unwrap_or(&1);
-    }
-
-    fn _update(
-        &mut self,
-        start_idx: usize,
-        end_idx: usize,
-        idx: usize,
-        value: i32,
-        tree_idx: usize,
-    ) {
-        if start_idx == end_idx {
-            self.tree[tree_idx] = value;
-            return;
+        pub fn update(&mut self, idx: usize, value: T) {
+            self._update(0, self.n - 1, idx, value, 1);
         }
 
-        let mid_idx = (start_idx + end_idx) / 2;
-
-        if idx <= mid_idx {
-            self._update(start_idx, mid_idx, idx, value, tree_idx * 2);
-        } else {
-            self._update(mid_idx + 1, end_idx, idx, value, tree_idx * 2 + 1);
+        pub fn query(&self, range: &RangeInclusive<usize>) -> T {
+            return self._query(0, self.n - 1, range, 1);
         }
 
-        self.tree[tree_idx] = self.tree.get(tree_idx * 2).unwrap_or(&1)
-            * self.tree.get(tree_idx * 2 + 1).unwrap_or(&1);
-    }
+        fn _build(&mut self, x: &Vec<T>, start_idx: usize, end_idx: usize, tree_idx: usize) {
+            if tree_idx >= self.tree.len() {
+                return;
+            }
 
-    fn _query(
-        &self,
-        start_idx: usize,
-        end_idx: usize,
-        query_range: &RangeInclusive<usize>,
-        tree_idx: usize,
-    ) -> i32 {
-        let mid_idx = (start_idx + end_idx) / 2;
+            if start_idx == end_idx {
+                self.tree[tree_idx] = x[start_idx];
+                return;
+            }
 
-        if tree_idx >= self.tree.len() {
-            return 1;
+            let mid_idx = (start_idx + end_idx) / 2;
+            let left_child_idx = tree_idx * 2;
+            let right_child_idx = tree_idx * 2 + 1;
+
+            self._build(x, start_idx, mid_idx, left_child_idx);
+            self._build(x, mid_idx + 1, end_idx, right_child_idx);
+
+            self.tree[tree_idx] = (self.operation)(
+                self.tree.get(left_child_idx).unwrap_or(&self.default_value),
+                self.tree
+                    .get(right_child_idx)
+                    .unwrap_or(&self.default_value),
+            );
         }
 
-        if *query_range.start() > end_idx || *query_range.end() < start_idx {
-            return 1;
+        fn _update(
+            &mut self,
+            start_idx: usize,
+            end_idx: usize,
+            idx: usize,
+            value: T,
+            tree_idx: usize,
+        ) {
+            if start_idx == end_idx {
+                self.tree[tree_idx] = value;
+                return;
+            }
+
+            let mid_idx = (start_idx + end_idx) / 2;
+            let left_child_idx = tree_idx * 2;
+            let right_child_idx = tree_idx * 2 + 1;
+
+            if idx <= mid_idx {
+                self._update(start_idx, mid_idx, idx, value, left_child_idx);
+            } else {
+                self._update(mid_idx + 1, end_idx, idx, value, right_child_idx);
+            }
+
+            self.tree[tree_idx] = (self.operation)(
+                self.tree.get(left_child_idx).unwrap_or(&self.default_value),
+                self.tree
+                    .get(right_child_idx)
+                    .unwrap_or(&self.default_value),
+            );
         }
 
-        if *query_range.start() <= start_idx && end_idx <= *query_range.end() {
-            return self.tree[tree_idx];
+        fn _query(
+            &self,
+            start_idx: usize,
+            end_idx: usize,
+            query_range: &RangeInclusive<usize>,
+            tree_idx: usize,
+        ) -> T {
+            let mid_idx = (start_idx + end_idx) / 2;
+
+            if tree_idx >= self.tree.len() {
+                return self.default_value;
+            }
+
+            if *query_range.start() > end_idx || *query_range.end() < start_idx {
+                return self.default_value;
+            }
+
+            if *query_range.start() <= start_idx && end_idx <= *query_range.end() {
+                return self.tree[tree_idx];
+            }
+
+            let left_child_idx = tree_idx * 2;
+            let right_child_idx = tree_idx * 2 + 1;
+            let left = self._query(start_idx, mid_idx, query_range, left_child_idx);
+            let right = self._query(mid_idx + 1, end_idx, query_range, right_child_idx);
+
+            (self.operation)(&left, &right)
         }
-
-        let left_child_idx = tree_idx * 2;
-        let right_child_idx = tree_idx * 2 + 1;
-        let left = self._query(start_idx, mid_idx, query_range, left_child_idx);
-        let right = self._query(mid_idx + 1, end_idx, query_range, right_child_idx);
-
-        left * right
     }
 }
 
@@ -157,7 +178,7 @@ fn main() {
 }
 
 fn solution(x: Vec<i32>, commands: Vec<Command>) -> String {
-    let mut tree = SegmentTree::new(&x);
+    let mut tree = segtree::SegmentTree::new(&x, 1, |a, b| a * b);
     let mut answer = String::new();
 
     for command in commands {
